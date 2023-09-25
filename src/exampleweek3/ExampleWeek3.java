34a430766a0d1244aea19a899f7d580e8c15efc0
sivagamasrinivasan <784108@pdsb.net>
2023-09-25 09:24:09 -0400

    week 3 example

---
+++ src/exampleweek3/ExampleWeek3.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package exampleweek3;
+
+/**
+ *
+ * @author Sivagama
+ */
+public class ExampleWeek3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+       System.out.println("Week 3 demo ");
+    }
+
+}
